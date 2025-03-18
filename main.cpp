5ec7997ea6e1b16bc6dbf6f54d241d482a6c0bc0
unknown <emanuel.bumbuta@gmail.com>
2025-03-18 19:47:25 +0200

    Add raylib library

--- main.cpp
+++ main.cpp
@@ -1,41 +1,14 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <raylib.h>
 
-using namespace sf;
 using namespace std;
 
-int main() {
-    RenderWindow window(VideoMode(500, 500), "Simple Window");
-    RectangleShape rect(Vector2f(160, 50));
+int main () {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
 
-    rect.setFillColor(Color::Green);
-    rect.setPosition((window.getSize().x - rect.getSize().x) / 2, (window.getSize().y - rect.getSize().y) / 2);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Atestat");
 
-    window.draw(rect);
-    window.display();
-
-    while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event)) {
-            for(int i = 0; i < 9999; i++)
-                i*=1;
-
-            rect.setPosition(rect.getPosition().x, rect.getPosition().y + 1);
-
-            window.clear();
-            window.draw(rect);
-            window.display();
-            
-            if (event.type == Event::Closed)
-                window.close();
-        }
-    }
-
+    CloseWindow();
     return 0;
-}
-
-void asteapta() {
-    for(int i = 0; i < 9999999999; i++)
-        i*=1;
-
 }
